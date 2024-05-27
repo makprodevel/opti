c508ce7303f3322a8fcb51148a1bd166da4df492
mak.pro.devel <mak.pro.devel@gmail.com>
2024-05-27 14:15:33 +0300

    upgrade models.py

--- opti/core/models.py
+++ opti/core/models.py
@@ -1,30 +1,37 @@
-from sqlalchemy import Column, String, Boolean, TIMESTAMP, UUID, func, ForeignKey, Text
+from typing import Annotated
+from sqlalchemy import ForeignKey, text
 from opti.core.database import DBase
-from sqlalchemy.orm import relationship
-import uuid
+from sqlalchemy.orm import relationship, mapped_column, Mapped
+from uuid import UUID
+from datetime import datetime
+
+
+uuidpk = Annotated[UUID, mapped_column(primary_key=True, server_default=text("uuid_generate_v4()"))]
+created_at_c = Annotated[datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))]
 
 
 class User(DBase):
     __tablename__ = "users"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    email = Column(String, nullable=False, unique=True)
-    nickname = Column(String, nullable=False)
-    is_superuser = Column(Boolean, nullable=False, default=False)
-    registered_at = Column(TIMESTAMP, default=func.now())
-    #online_at = Column(TIMESTAMP, default=func.now())
-    is_blocked = Column(Boolean, nullable=False, default=False)
+    id: Mapped[uuidpk]
+    email: Mapped[str] = mapped_column(unique=True)
+    nickname: Mapped[str] = mapped_column()
+    is_superuser: Mapped[bool] = mapped_column(server_default=text("false"))
+    registered_at: Mapped[created_at_c]
+    is_blocked: Mapped[bool] = mapped_column(server_default=text("false"))
 
 
 class Message(DBase):
     __tablename__ = "message"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    sender_id = Column(UUID(as_uuid=True), ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    id: Mapped[uuidpk]
+    sender_id: Mapped[UUID] = mapped_column( ForeignKey('users.id', ondelete='CASCADE'))
+    recipient_id: Mapped[UUID] = mapped_column( ForeignKey('users.id', ondelete='CASCADE'))
+    message: Mapped[str] = mapped_column()
+    created_at: Mapped[created_at_c]
+    is_viewed: Mapped[bool] = mapped_column(server_default=text("false"))
     sender = relationship('User', back_populates='message')
-    recipient_id = Column(UUID(as_uuid=True), ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
     recipient = relationship('User', back_populates='message')
-    message = Column(Text, nullable=False)
 
 
 User.messages_sent = relationship('Message', back_populates='sender', foreign_keys=[Message.sender_id])
