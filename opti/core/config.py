a66f1b417e499c62961f96c2fe0416e64b3f517f
mak <mak.pro.devel@gmail.com>
2024-08-22 17:16:01 +0300

    fix config

--- opti/core/config.py
+++ opti/core/config.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 from loguru import logger
 
-if not os.environ.get('release') == 'yes':
+if 'RELEASE' not in os.environ:
     load_dotenv()
 
 logger.add('logs/log.txt', rotation="500 KB")
@@ -13,14 +13,14 @@ origins = [
 GOOGLE_REDIRECT_URI = "http://localhost:8000/auth/google"
 GOOGLE_CERTS_URL = "https://www.googleapis.com/oauth2/v3/certs"
 GOOGLE_CERTS_TTL = 60
-GOOGLE_CLIENT_ID = os.environ.get('client_id')
-GOOGLE_CLIENT_SECRET = os.environ.get('client_secret')
-SECRET_KEY = os.environ.get('secret_key')
-DB_HOST = os.environ.get("db_host")
-DB_PORT = os.environ.get("db_port", 5432)
-DB_NAME = os.environ.get("db_name", "opti")
-DB_USER = os.environ.get("db_user", "postgres") 
-DB_PASS = os.environ.get("db_pass")
+GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
+GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DB_HOST = os.environ.get("POSTGRESQL_HOST")
+DB_PORT = os.environ.get("POSTGRESQL_PORT", 5432)
+DB_NAME = os.environ.get("POSTGRESQL_NAME", "opti")
+DB_USER = os.environ.get("POSTGRESQL_USER", "postgres") 
+DB_PASS = os.environ.get("POSTGRESQL_PASS")
 API_ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 30
 REDIS_URL = os.environ.get("redis_url", "redis://localhost:6379")
 REDIS_DB = os.environ.get("redis_db", 0)
